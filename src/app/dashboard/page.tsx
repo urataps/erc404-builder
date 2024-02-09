fe12a2a4ee2d5b34529e06e11e746206eee0db7e
doinel1a <doinel1atanasiu@gmail.com>
2024-02-09 21:10:25 +0100

    feat(dashboard): implement partial ui

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
 
+import { PencilRuler } from 'lucide-react';
+import Link from 'next/link';
+
 import Main from '@/components/main';
+import Section from '@/components/section';
+import { Button } from '@/components/ui/button';
+import { ERoutesName, ERoutesPath } from '@/constants/routes';
 
 export default function Dashboard() {
   return (
-    <Main>
-      <h1 className='text-2xl font-bold'>Dashboard page</h1>
+    <Main className='justify-start'>
+      <Section title='Collections'>
+        <div className='border-boder flex h-24 w-full items-center gap-x-5 rounded-md border p-5 transition-[background-color_transform] hover:-translate-y-1 hover:bg-secondary'>
+          <div className='justify-cetner flex h-10 w-10 items-center rounded-full bg-primary p-2.5'>
+            <PencilRuler className='h-10 w-10 text-primary-foreground' />
+          </div>
+
+          <div className='flex w-full items-center justify-between'>
+            <p>You have not created any collection yet.</p>
+
+            <Button asChild>
+              <Link href={ERoutesPath.create}>{ERoutesName.create}</Link>
+            </Button>
+          </div>
+        </div>
+      </Section>
     </Main>
   );
 }
---
+++ src/components/section.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import type { ComponentProps } from 'react';
+
+import { cn } from '@/lib/utils';
+
+type TSection = ComponentProps<'section'> & {
+  title: string;
+  subtitle?: string;
+};
+
+export default function Section({
+  title,
+  subtitle,
+  children,
+  className,
+  ...otherProperties
+}: TSection) {
+  const id = title.replaceAll(' ', '-').toLowerCase();
+  const headerId = `${id}-title`;
+
+  return (
+    <section
+      id={id}
+      aria-labelledby={headerId}
+      className={cn('flex w-full max-w-7xl flex-col gap-y-5 p-5', className)}
+      {...otherProperties}
+    >
+      <header>
+        <h2 id={headerId} className='text-3xl font-medium'>
+          {title}
+        </h2>
+        {subtitle ? <h3 className='text-2xl'>{subtitle}</h3> : null}
+      </header>
+
+      {children}
+    </section>
+  );
+}
