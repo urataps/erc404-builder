91143d25e4c1d6c776ae098b2b89e50f2055d1bc
doinel1a <doinel1atanasiu@gmail.com>
2024-02-10 16:33:07 +0100

    refactor(dashboard/deploy-contract): extract navbar into dashboard/_components folder

--- src/app/dashboard/deploy-contract/layout.tsx
+++ src/app/dashboard/deploy-contract/layout.tsx
@@ -3,16 +3,17 @@ import React from 'react';
 import type { PropsWithChildren } from 'react';
 
 import Main from '@/components/main';
+import { ERoutesPath } from '@/constants/routes';
 
+import Navbar from '../_components/navbar';
 import DeployContractAside from './_components/deploy-contract-aside';
-import Navbar from './_components/navbar';
 
 type TLayout = PropsWithChildren;
 
 export default function Layout({ children }: TLayout) {
   return (
     <>
-      <Navbar />
+      <Navbar title='Drop a collection' backTo={ERoutesPath.create} />
 
       <Main className='flex-row items-start gap-x-5'>
         <div className='w-2/3'>{children}</div>
