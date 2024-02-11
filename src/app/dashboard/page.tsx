d0d287ba4aa3583b6843a9bc1c7c7f2173bf4f50
doinel1a <doinel1atanasiu@gmail.com>
2024-02-11 11:51:45 +0100

    feat(dashboard): implement aside partner nft generator platform

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
 import { PencilRuler } from 'lucide-react';
+import Image from 'next/image';
 import Link from 'next/link';
 
+import theNftGeneratorLogo from '@/assets/images/the-nft-generator-logo.svg';
+import Aside from '@/components/aside';
 import Main from '@/components/main';
 import Section from '@/components/section';
 import { Button } from '@/components/ui/button';
@@ -10,10 +13,18 @@ import { ERoutesName, ERoutesPath } from '@/constants/routes';
 
 import Layout from '../_components/layout';
 
+const nftPartnerLinks = [
+  {
+    logo: theNftGeneratorLogo as string,
+    name: 'The NFT Generator',
+    link: 'https://the-nft-generator.com/?via=defibuilder'
+  }
+];
+
 export default function Dashboard() {
   return (
     <Layout>
-      <Main className='justify-start'>
+      <Main className='flex-row items-start'>
         <Section title='Collections'>
           <div className='border-boder flex h-24 w-full items-center gap-x-5 rounded-md border p-5 transition-[background-color_transform] hover:-translate-y-1 hover:bg-secondary'>
             <div className='justify-cetner flex h-10 w-10 items-center rounded-full bg-primary p-2.5'>
@@ -29,6 +40,32 @@ export default function Dashboard() {
             </div>
           </div>
         </Section>
+
+        <Aside className='mt-10'>
+          <h3 className='text-semibold text-lg'>Need to generate your own NFTs?</h3>
+          <p className='mt-1.5'>
+            Get started using one of the best <span className='font-medium'>no coding</span> NFTs
+            generator platforms:
+          </p>
+
+          <ul className='mt-5'>
+            {nftPartnerLinks.map((partner) => (
+              <li key={partner.name}>
+                <Button variant='link' className='px-0 py-0 text-foreground' asChild>
+                  <Link href={partner.link} className='flex gap-x-2.5 text-sm'>
+                    <Image
+                      src={partner.logo}
+                      alt={`${partner.name}'s logo`}
+                      width={25}
+                      height={25}
+                    />
+                    {partner.name}
+                  </Link>
+                </Button>
+              </li>
+            ))}
+          </ul>
+        </Aside>
       </Main>
     </Layout>
   );
