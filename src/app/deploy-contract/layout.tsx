79b84f7aa8f1beea07eb2d37fe545255cd42129a
doinel1a <doinel1atanasiu@gmail.com>
2024-02-12 18:58:01 +0100

    refactor: pages to use shared navbar & footer through layout

--- src/app/deploy-contract/layout.tsx
+++ src/app/deploy-contract/layout.tsx
@@ -5,29 +5,23 @@ import type { PropsWithChildren } from 'react';
 import Aside from '@/components/aside';
 import FeatureBanner from '@/components/feature-banner';
 import Main from '@/components/main';
-import { ERoutesPath } from '@/constants/routes';
 
-import Navbar from '../dashboard/_components/navbar';
 import DeployContractAside from './_components/deploy-contract-aside';
 
 type TLayout = PropsWithChildren;
 
 export default function Layout({ children }: TLayout) {
   return (
-    <>
-      <Navbar title='Drop a collection' backTo={ERoutesPath.dashboard} />
+    <Main className='flex-row items-start gap-x-5 overflow-y-auto'>
+      <div className='w-2/3'>{children}</div>
 
-      <Main className='flex-row items-start gap-x-5 overflow-y-auto'>
-        <div className='w-2/3'>{children}</div>
+      <div className='mt-10 flex w-1/3 flex-col gap-y-5'>
+        <DeployContractAside />
 
-        <div className='mt-10 flex w-1/3 flex-col gap-y-5'>
-          <DeployContractAside />
-
-          <Aside>
-            <FeatureBanner />
-          </Aside>
-        </div>
-      </Main>
-    </>
+        <Aside>
+          <FeatureBanner />
+        </Aside>
+      </div>
+    </Main>
   );
 }
