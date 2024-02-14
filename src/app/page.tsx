e538f50e80f804e5129501a495534fbbafdc7ce2
doinel1a <doinel1atanasiu@gmail.com>
2024-02-14 17:53:07 +0100

    fix(home): typos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,10 +9,10 @@ export default function Home() {
       <Main className='mx-auto max-w-7xl justify-center gap-y-2.5'>
         <h1 className='text-3xl font-bold'>Free ERC404 token generator</h1>
         <h2 className='text-center text-2xl font-medium'>
-          Generate custom ERC404 token using tested Smart contract. <br />
+          Generate a custom ERC404 token using tested Smart Contracts. <br />
           Deploy ERC404 directly from your wallet using metamask. <br />
-          Create ERC404 token on Binance Smart Chain, Linea, Arbitrum and many other EVM
-          blockchains.
+          Create ERC404 tokens on Binance Smart Chain, Linea, Arbitrum and many other EVM
+          Blockchains.
         </h2>
       </Main>
     </WithSignTAC>
