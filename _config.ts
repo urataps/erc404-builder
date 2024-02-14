d6acfb2396f6014fbcb32f8faa383f8930269293
doinel1a <doinel1atanasiu@gmail.com>
2024-02-14 17:04:55 +0100

    refactor: update og metadata + add title & subtitle to homepage

--- _config.ts
+++ _config.ts
@@ -1,9 +1,13 @@
 const config = {
   metadata: {
+    domain: 'erc404.defibuilder.com',
     title: 'DeFi Builder - Free ERC404 token generator',
     description:
       'Create a free ERC404 token and deploy on most blockchains. Support Binance Smart Chain, Arbitrum, Linea and Polygon. Deploy free ERC404 token using metamask wallet.',
-    keywords: 'free ERC404 token, what is ERC404, how to deploy ERC404, ERC404 token'
+    keywords: 'free ERC404 token, what is ERC404, how to deploy ERC404, ERC404 token',
+    icons: 'favicon.svg',
+    manifest: 'app.webmanifest',
+    ogImage: 'og.png'
   },
   server: {
     host: 'localhost',
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,14 @@ import WithSignTAC from '@/higher-order-components/with-sign-tac';
 export default function Home() {
   return (
     <WithSignTAC>
-      <Main className='justify-center'>
-        <h1 className='text-2xl font-bold'>Home page</h1>
+      <Main className='mx-auto max-w-7xl justify-center gap-y-2.5'>
+        <h1 className='text-3xl font-bold'>Free ERC404 token generator</h1>
+        <h2 className='text-center text-2xl font-medium'>
+          Generate custom ERC404 token using tested Smart contract. <br />
+          Deploy ERC404 directly from your wallet using metamask. <br />
+          Create ERC404 token on Binance Smart Chain, Linea, Arbitrum and many other EVM
+          blockchains.
+        </h2>
       </Main>
     </WithSignTAC>
   );
