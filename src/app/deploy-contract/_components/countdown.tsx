eebdf1aa36691e27dc988206830e1f7f8b02dc9b
doinel1a <doinel1atanasiu@gmail.com>
2024-02-14 17:09:50 +0100

    chore: add 24hs to countdown

--- src/app/deploy-contract/_components/countdown.tsx
+++ src/app/deploy-contract/_components/countdown.tsx
@@ -6,7 +6,7 @@ import { differenceInSeconds } from 'date-fns';
 
 const Countdown: React.FC = () => {
   const [endTime] = useState(() => {
-    const now = new Date('2024-02-13 15:45');
+    const now = new Date('2024-02-14 17:00');
     const end = new Date(now);
     end.setSeconds(now.getSeconds() + 24 * 60 * 60);
     return end;
