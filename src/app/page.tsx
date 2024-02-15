51b580f26fb129dec72b426f088131d73051e6a5
doinel1a <doinel1atanasiu@gmail.com>
2024-02-15 14:41:17 +0100

    refactor(home): add wallet button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
 
 import Main from '@/components/main';
+import WalletButton from '@/components/navbar/wallet-button';
 import WithSignTAC from '@/higher-order-components/with-sign-tac';
 
 export default function Home() {
   return (
     <WithSignTAC>
       <Main className='mx-auto max-w-7xl justify-center gap-y-2.5'>
-        <h1 className='text-3xl font-bold'>Free ERC404 token generator</h1>
+        <h1 className='text-center text-3xl font-bold'>Free ERC404 token generator</h1>
         <h2 className='text-center text-2xl font-medium'>
           Generate a custom ERC404 token using tested Smart Contracts. <br />
           Deploy ERC404 directly from your wallet using metamask. <br />
           Create ERC404 tokens on Binance Smart Chain, Linea, Arbitrum and many other EVM
           Blockchains.
         </h2>
+
+        <WalletButton className='mt-5' />
       </Main>
     </WithSignTAC>
   );
