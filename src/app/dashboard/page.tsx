4f0b33f16cf1f87bb6a1ba0fc84ce2498d3cf529
doinel1a <doinel1atanasiu@gmail.com>
2024-02-16 14:37:31 +0100

    refactor: remove required auth from routes

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import config from '_config';
 
 import Section from '@/components/section';
-import WithAuthentication from '@/higher-order-components/with-authentication';
 import WithSignTAC from '@/higher-order-components/with-sign-tac';
 
 import Collections from './_components/collections-list';
@@ -14,12 +13,10 @@ export const metadata = {
 
 export default function Dashboard() {
   return (
-    <WithAuthentication>
-      <WithSignTAC>
-        <Section title='Collections' className='relative h-full'>
-          <Collections />
-        </Section>
-      </WithSignTAC>
-    </WithAuthentication>
+    <WithSignTAC>
+      <Section title='Collections' className='relative h-full'>
+        <Collections />
+      </Section>
+    </WithSignTAC>
   );
 }
