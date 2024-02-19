1cc1d2250ff3138d2350bfd6798555aa9fe0b019
urataps <alexeluca.spataru@gmail.com>
2024-02-19 10:51:35 +0200

    refactor: client folder

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -29,7 +29,7 @@ module.exports = {
   rules: {
     // base
     indent: ['error', 2, { SwitchCase: 1 }],
-    'linebreak-style': ['error', 'windows'],
+    'linebreak-style': ['error', 'unix'],
     quotes: ['error', 'single'],
     semi: ['error', 'always'],
     // end
@@ -61,3 +61,4 @@ module.exports = {
     // end
   }
 };
+
